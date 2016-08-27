a960775f8efca36cc0125195990397b74f11fa3d
Mickyjou <michaelba0901@gmail.com>
2016-08-27 12:42:45 +0200

    Added a permission to buy the PaintballGun

--- src/main/java/de/mickyjou/paintballgun/PaintballCommand.java
+++ src/main/java/de/mickyjou/paintballgun/PaintballCommand.java
@@ -13,27 +13,32 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class PaintballCommand implements CommandExecutor {
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (sender instanceof Player) {
-            if (Bukkit.getServicesManager().isProvidedFor(GemProvider.class)) {
-                GemProvider gemProvider = Bukkit.getServicesManager().getRegistration(GemProvider.class).getProvider();
-                if (!gemProvider.removeGems(((OfflinePlayer) sender), 10)) {
-                    sender.sendMessage(ChatColor.RED + "You don't have enough Gems to buy a paintball gun.");
-                    return true;
-                }
-            }
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (sender instanceof Player) {
+			Player p = (Player) sender;
+			if (p.hasPermission("paintballgun.buy")) {
+				if (Bukkit.getServicesManager().isProvidedFor(GemProvider.class)) {
+					GemProvider gemProvider = Bukkit.getServicesManager().getRegistration(GemProvider.class)
+							.getProvider();
+					if (!gemProvider.removeGems(((OfflinePlayer) sender), 10)) {
+						sender.sendMessage(ChatColor.RED + "You don't have enough Gems to buy a paintball gun.");
+						return true;
+					}
+				}
 
-            Player p = (Player) sender;
-            ItemStack itemstack = new ItemStack(Material.DIAMOND_HOE);
-            ItemMeta meta = itemstack.getItemMeta();
-            meta.setDisplayName(ChatColor.DARK_PURPLE + "Paintball Gun");
-            itemstack.setItemMeta(meta);
-            p.getInventory().addItem(itemstack);
+				ItemStack itemstack = new ItemStack(Material.DIAMOND_HOE);
+				ItemMeta meta = itemstack.getItemMeta();
+				meta.setDisplayName(ChatColor.DARK_PURPLE + "Paintball Gun");
+				itemstack.setItemMeta(meta);
+				p.getInventory().addItem(itemstack);
 
-            return true;
-        } else {
-            return false;
-        }
-    }
+				return true;
+			}else{
+				p.sendMessage(ChatColor.RED + "You don't have the permission to buy a PaintballGun.");
+			}
+		} else {
+			return false;
+		}
+	}
 }
