a82c6fa76af1a37023aeb7464fb065e329872efd
Mickyjou <michaelba0901@gmail.com>
2016-08-27 12:50:16 +0200

    Added config.yml to change the price of the PaintballGun

--- src/main/java/de/mickyjou/paintballgun/PaintballCommand.java
+++ src/main/java/de/mickyjou/paintballgun/PaintballCommand.java
@@ -8,20 +8,29 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class PaintballCommand implements CommandExecutor {
+
+	File file = new File("plugins/PaintballGun", "config.yml");
+	FileConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (sender instanceof Player) {
 			Player p = (Player) sender;
 			if (p.hasPermission("paintballgun.buy")) {
+
+				String gems = cfg.getString("gems");
+
 				if (Bukkit.getServicesManager().isProvidedFor(GemProvider.class)) {
 					GemProvider gemProvider = Bukkit.getServicesManager().getRegistration(GemProvider.class)
 							.getProvider();
-					if (!gemProvider.removeGems(((OfflinePlayer) sender), 10)) {
+					if (!gemProvider.removeGems(((OfflinePlayer) sender), gems)) {
 						sender.sendMessage(ChatColor.RED + "You don't have enough Gems to buy a paintball gun.");
 						return true;
 					}
@@ -34,7 +43,7 @@ public class PaintballCommand implements CommandExecutor {
 				p.getInventory().addItem(itemstack);
 
 				return true;
-			}else{
+			} else {
 				p.sendMessage(ChatColor.RED + "You don't have the permission to buy a PaintballGun.");
 			}
 		} else {
