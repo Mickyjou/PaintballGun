27c9289f1900cfdd249ceeabad9de006ebfb4087
Mickyjou <michaelba0901@gmail.com>
2016-08-27 12:49:16 +0200

    Update PaintballGunPlugin.java

--- src/main/java/de/mickyjou/paintballgun/PaintballGunPlugin.java
+++ src/main/java/de/mickyjou/paintballgun/PaintballGunPlugin.java
@@ -2,22 +2,43 @@ package de.mickyjou.paintballgun;
 
 import de.mickyjou.paintballgun.listeners.PlayerInteractListener;
 import de.mickyjou.paintballgun.listeners.ProjectileHitListener;
+
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PaintballGunPlugin extends JavaPlugin {
-    @Override
-    public void onEnable() {
-        registerCommands();
-        registerEvents();
-    }
 
-    private void registerCommands() {
-        de.mickyjou.paintballgun.PaintballCommand cpb = new PaintballCommand();
-        getCommand("paintballgun").setExecutor(cpb);
-    }
+	File file = new File("plugins/PaintballGun", "config.yml");
+	FileConfiguration cfg = YamlConfiguration.loadConfiguration(file);
 
-    private void registerEvents() {
-        getServer().getPluginManager().registerEvents(new PlayerInteractListener(this), this);
-        getServer().getPluginManager().registerEvents(new ProjectileHitListener(this), this);
-    }
+	@Override
+	public void onEnable() {
+		fillConfig();
+		registerCommands();
+		registerEvents();
+	}
+
+	private void registerCommands() {
+		de.mickyjou.paintballgun.PaintballCommand cpb = new PaintballCommand();
+		getCommand("paintballgun").setExecutor(cpb);
+	}
+
+	private void registerEvents() {
+		getServer().getPluginManager().registerEvents(new PlayerInteractListener(this), this);
+		getServer().getPluginManager().registerEvents(new ProjectileHitListener(this), this);
+	}
+
+	private void fillConfig() {
+		if (!file.exists()) {
+			cfg.set("gems", 10);
+			try {
+				cfg.save(file);
+			} catch (Exception e) {
+
+			}
+		}
+	}
 }
